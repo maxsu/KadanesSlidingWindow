441612354aa963cd2ef07af0b12d5b9032d0e9ee
Max Suica <max.suica@gmail.com>
2023-10-24 16:43:15 -0700

    refactor(PhiTestCodeImplemenation): Place use case into an `if __name__ == "__main__":` block

--- src/kadane/PhiTestCodeImplementation.py
+++ src/kadane/PhiTestCodeImplementation.py
@@ -19,45 +19,47 @@ FOLLOW_UP_PROMPT = "After Alice fell down the rabbit hole, she..."
 MAX_LEN = 1000
 MODEL = "microsoft/phi-1_5"
 
-# Create a Kadane Sliding Window with a size of MAX_LEN tokens
-kadane_sliding_window = KadaneSlidingWindow(maxlen=MAX_LEN)
 
-# Initialize the Causal Language Model and Tokenizer
-model = AutoModelForCausalLM.from_pretrained(
-    MODEL,
-    trust_remote_code=True,
-    torch_dtype="auto",
-)
-tokenizer = AutoTokenizer.from_pretrained(
-    MODEL,
-    trust_remote_code=True,
-    torch_dtype="auto",
-)
+if __name__ == "__main__":
+    # Create a Kadane Sliding Window with a size of MAX_LEN tokens
+    kadane_sliding_window = KadaneSlidingWindow(maxlen=MAX_LEN)
 
-# Add the prompt to the sliding window before starting the loop
-kadane_sliding_window.add(PROMPT.split())
-
-generated_text = ""
-while len(generated_text) < 100:
-    # Ask the model a follow-up prompt
-    context = FOLLOW_UP_PROMPT + kadane_sliding_window.get_context()
-
-    # Encode the context using the tokenizer
-    input_ids = tokenizer(context, return_tensors="pt").input_ids
-
-    # Generate text using the model with the encoded context as input
-    output_ids = model.generate(
-        input_ids=input_ids,
-        max_length=min(100, len(input_ids) + 75),
+    # Initialize the Causal Language Model and Tokenizer
+    model = AutoModelForCausalLM.from_pretrained(
+        MODEL,
+        trust_remote_code=True,
+        torch_dtype="auto",
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        MODEL,
+        trust_remote_code=True,
+        torch_dtype="auto",
     )
 
-    # Decode the generated text using the tokenizer
-    generated_text += tokenizer.decode(
-        output_ids[0],
-        skip_special_tokens=True,
-    )
+    # Add the prompt to the sliding window before starting the loop
+    kadane_sliding_window.add(PROMPT.split())
 
-    # Add the generated text to the sliding window
-    kadane_sliding_window.add(generated_text.split())
+    generated_text = ""
+    while len(generated_text) < 100:
+        # Ask the model a follow-up prompt
+        context = FOLLOW_UP_PROMPT + kadane_sliding_window.get_context()
 
-print(generated_text)
+        # Encode the context using the tokenizer
+        input_ids = tokenizer(context, return_tensors="pt").input_ids
+
+        # Generate text using the model with the encoded context as input
+        output_ids = model.generate(
+            input_ids=input_ids,
+            max_length=min(100, len(input_ids) + 75),
+        )
+
+        # Decode the generated text using the tokenizer
+        generated_text += tokenizer.decode(
+            output_ids[0],
+            skip_special_tokens=True,
+        )
+
+        # Add the generated text to the sliding window
+        kadane_sliding_window.add(generated_text.split())
+
+    print(generated_text)
