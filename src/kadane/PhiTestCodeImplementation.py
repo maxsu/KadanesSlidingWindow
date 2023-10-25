085c0f3fae41a635be1cb1829be068b4b46a571b
Max Suica <max.suica@gmail.com>
2023-10-24 17:09:33 -0700

    refactor(PhiTestCodeImplementation.py): Extract and document the PhiTestCodeImplmentation usecase refactor(PhiTestCodeImplementation.py): Add types to main section refactor(PhiTestCodeImplementation.py): Rename variable `kadanes_sliding_window` to `window`

--- src/kadane/PhiTestCodeImplementation.py
+++ src/kadane/PhiTestCodeImplementation.py
@@ -20,29 +20,26 @@ MAX_LEN = 1000
 MODEL = "microsoft/phi-1_5"
 
 
-if __name__ == "__main__":
-    # Create a Kadane Sliding Window with a size of MAX_LEN tokens
-    kadane_sliding_window = KadaneSlidingWindow(maxlen=MAX_LEN)
+def PhiTestCodeImplementation(
+    window: KadaneSlidingWindow,
+    model: AutoModelForCausalLM,
+    tokenizer: AutoTokenizer,
+):
+    """
+    Generate text using the Phi model.
 
-    # Initialize the Causal Language Model and Tokenizer
-    model = AutoModelForCausalLM.from_pretrained(
-        MODEL,
-        trust_remote_code=True,
-        torch_dtype="auto",
-    )
-    tokenizer = AutoTokenizer.from_pretrained(
-        MODEL,
-        trust_remote_code=True,
-        torch_dtype="auto",
-    )
-
+    Args:
+        window (KadaneSlidingWindow): The sliding window to use for the Phi model.
+        model (AutoModelForCausalLM): The Phi model.
+        tokenizer (AutoTokenizer): The tokenizer for the Phi model.
+    """
     # Add the prompt to the sliding window before starting the loop
-    kadane_sliding_window.add(PROMPT.split())
+    window.add(PROMPT.split())
 
     generated_text = ""
     while len(generated_text) < 100:
         # Ask the model a follow-up prompt
-        context = FOLLOW_UP_PROMPT + kadane_sliding_window.get_context()
+        context = FOLLOW_UP_PROMPT + window.get_context()
 
         # Encode the context using the tokenizer
         input_ids = tokenizer(context, return_tensors="pt").input_ids
@@ -60,6 +57,30 @@ if __name__ == "__main__":
         )
 
         # Add the generated text to the sliding window
-        kadane_sliding_window.add(generated_text.split())
+        window.add(generated_text.split())
 
     print(generated_text)
+
+
+if __name__ == "__main__":
+    # Create a Kadane Sliding Window with a size of MAX_LEN tokens
+    window: KadaneSlidingWindow = KadaneSlidingWindow(maxlen=MAX_LEN)
+
+    # Initialize the Causal Language Model and Tokenizer
+    model: AutoModelForCausalLM = AutoModelForCausalLM.from_pretrained(
+        MODEL,
+        trust_remote_code=True,
+        torch_dtype="auto",
+    )
+    tokenizer: AutoTokenizer = AutoTokenizer.from_pretrained(
+        MODEL,
+        trust_remote_code=True,
+        torch_dtype="auto",
+    )
+
+    # Generate text using the Phi model and the Kadane Sliding Window
+    PhiTestCodeImplementation(
+        window=window,
+        model=model,
+        tokenizer=tokenizer,
+    )
